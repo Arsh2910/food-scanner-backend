95f671f65b3d9eb095bb1c5af9dd3a67ee055cfc
Arsh <arshj347@gmail.com>
2026-02-23 00:07:47 +0530

    Updated auth flow and profile routes

--- routes/ingredientRoutes.js
+++ routes/ingredientRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Ingredient = require("../models/Ingredient");
-
-router.post("/add", async (req, res) => {
+const protect = require("../middleware/authMiddleware");
+router.post("/add", protect, async (req, res) => {
   try {
     const ingredient = await Ingredient.create(req.body);
     res.status(201).json(ingredient);
