c105f9d1d60985700398b86bc50c620a72717ed3
Arsh <arshj347@gmail.com>
2026-02-23 07:35:17 +0530

    Added age, gender, healthIssues, likes to profile

--- models/User.js
+++ models/User.js
@@ -4,9 +4,18 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    age: { type: Number, min: 1, max: 120, default: null },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other", ""],
+      default: "",
+    },
     diet: { type: String, default: "" },
     allergies: { type: [String], default: [] },
+    healthIssues: { type: [String], default: [] },
+    likes: { type: [String], default: [] },
     avoid: { type: [String], default: [] },
+    preferencesCompleted: { type: Boolean, default: false },
   },
   { timestamps: true },
 );
